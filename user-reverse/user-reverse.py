e5682f86fd32cdbbde3ca91a596de92998ddb011
support hives <supporthives@supports-MacBook-Pro.local>
2021-09-23 20:01:49 +0530

    UserReverse an array

---
+++ user-reverse/user-reverse.py
@@ -0,0 +1,10 @@
+arr = []
+
+n = int(input("enter tyhe length of the arrray:"))
+for i in range(n):
+    user = int(input("Insert number:"))
+    arr.append(user)
+
+a = arr.reverse()
+print(arr)
+print(a)
